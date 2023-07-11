200b4a7c2d8bb6f89922e4c00ea2c1411982e879
Lee SangHun <mysh1105@gmail.com>
2023-07-11 14:11:43 +0900

    chore : make a folder tree
    - 3 layered architecture로 변경하기 위한 폴더구조 생성
    - controller, repository, service 3단계로 구성

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
+const usersRouter = require("./routes/users.route");
+
 app.use(express.json());
-app.use("/");
+app.use(cookieParser());
+app.use("/", [usersRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
---
+++ routes/users.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const { User } = require("../models");
+
+module.exports = router;
